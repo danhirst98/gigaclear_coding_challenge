923571f51d576c36b27a29215d9a71b97b70b12b
Dan Hirst <dan.hirst@earthi.co.uk>
2021-04-20 12:40:09 +0100

    Added comments to rate_card.py and main.py

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
 from networkx import read_graphml
 from rate_card import generate_rate_card
-from numbers import Number
 import argparse
 
 
 def main(rate_card_path, graph_path):
+    # Initialise total cost
     total_cost = 0
-    rate_card, min_weights = generate_rate_card(rate_card_path)
+
+    # read graphml file into networkx graph
     with open(graph_path) as file:
         graph = read_graphml(file)
-    print()
 
+    # generate rate_card and minimum distances between nodes
+    rate_card, min_weights = generate_rate_card(rate_card_path, graph)
+
+    # iterate through all nodes
     for node_name, node_type in graph.nodes(data="type"):
-        cost = rate_card[node_type]
-        if isinstance(cost, Number):
-            total_cost += cost
-        elif isinstance(cost, str):
+        # find cost structure from rate card
+        cost = rate_card[node_type.lower()]
+        # if cost is a flat rate, add it to total cost
+        if cost.isnumeric():
+            total_cost += float(cost)
+        # if cost is based on minimum distance between node and another node of certain type (e.g. cabinet)
+        # lookup minimum distance from min_weights and calculate cost
+        elif 'x' in cost:
             multiplier, source = cost.split('x')
-            total_cost += multiplier * min_weights[source][node_name]
+            total_cost += float(multiplier) * min_weights[source.lower()][node_name]
+        else:
+            raise NotImplementedError("")
 
+    # iterate through edges
     for edge in graph.edges(data=True):
+        # get material and length from graph data
         material = edge[2]['material']
         length = float(edge[2]['length'])
-        total_cost += rate_card[material] * length
+        # add cost to material based on multiplier * length
+        total_cost += float(rate_card[material]) * length
 
-    total_cost = sum(map(lambda x: x.get_total_cost(), rate_card.values()))
     return total_cost
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="This program takes a given graph and rate card and calculates the total cost of the network")
+    parser = argparse.ArgumentParser(description="This program takes a given graph and rate card and calculates the "
+                                                 "total cost of the network")
     parser.add_argument("-r", "--rate_card", help="path to rate card csv")
     parser.add_argument("-g", "--graph", help="path to graph file")
 
     args = parser.parse_args()
-    rate_card_path = args.rate_card
-    graph_path = args.graph
+    rate_card_path_arg = args.rate_card
+    graph_path_arg = args.graph
 
-    if not rate_card_path.endswith('.csv'):
+    # check that rate_card is a csv
+    if not rate_card_path_arg.endswith('.csv'):
         raise TypeError("Rate Card must be a csv file")
 
-    if not graph_path.endswith('.graphml'):
+    # check that graph is a .graphml file
+    if not graph_path_arg.endswith('.graphml'):
         raise TypeError("Graph must be a .graphml file")
 
-    print(main(rate_card_path, graph_path))
+    print(main(rate_card_path_arg, graph_path_arg))
--- rate_card.py
+++ rate_card.py
@@ -1,55 +1,51 @@
-from Edge import Edge
-from Node import Node
 import csv
 import re
 from networkx import multi_source_dijkstra_path_length
 
 
-def get_item_type(item):
-    return Edge if "/m" in item else Node
+def get_nodes(graph, node_type) -> list:
+    """
+    Returns list of node names that are of node type
 
-
-
-def get_nodes(graph, node_type):
+    :param graph: weighted graph we are looking at
+    :type graph: networkx.Graph
+    :param node_type: node type we are looking for
+    :type node_type: str
+    :return: list of node names that are of type node_type
+    :rtype: list
+    """
     return [x for x, y in graph.nodes(data=True) if y['type'].lower() == node_type]
 
 
 def generate_rate_card(rate_card_csv_path, graph) -> tuple:
+    # initialise rate_card and min_weights hash tables
     rate_card = {}
     min_weights = {}
+    # open rate card
     with open(rate_card_csv_path) as csvfile:
         rate_card_csv = csv.reader(csvfile, delimiter=',')
-        next(rate_card_csv, None)  # skip header of file
+        # skip header of file
+        next(rate_card_csv, None)
+        # iterate through rate card items
         for item, cost in rate_card_csv:
-            print(cost)
-            if cost.strip().isnumeric():
-                cost = float(cost)
-            elif 'x' in cost:
-                cost = cost
+            # strip whitespace from cost
+            cost = cost.strip()
+            # check if cost is based on multiplier
+            if 'x' in cost:
+                # find the source to find the min distance from
                 _, source = cost.split('x')
-                min_weights[source] = multi_source_dijkstra_path_length(graph, get_nodes(graph, source), weight='length')
-            else:
-                raise NotImplementedError("")
+                # use djikstras algorithm to find minimum weights between nodes of type source and all other nodes
+                # save in hash table for fast look up and so we don't have to calculate again
+                min_weights[source.lower()] = multi_source_dijkstra_path_length(graph,
+                                                                                get_nodes(graph, source.lower()),
+                                                                                weight='length')
+            # raise error if cost is not a number
+            elif not cost.isnumeric():
+                raise NotImplementedError(f"Rate card gave a cost in an unknown format. Must be a number or a string "
+                                          f"in the form<number>x<node_type>. Given cost was: {cost}")
+            # find the name of item (the text inside the parentheses if an edge, otherwise just the item)
             name = re.findall(r'\((.*?)\)', item)[0].lower() if "/m" in item else item.lower()
+            # add item to rate card
             rate_card[name] = cost
 
     return rate_card, min_weights
-
-# def generate_rate_card(rate_card_csv_path) -> dict:
-#     rate_card = {}
-#     with open(rate_card_csv_path) as csvfile:
-#         rate_card_csv = csv.reader(csvfile, delimiter=',')
-#         next(rate_card_csv, None)  # skip header of file
-#         for item, cost in rate_card_csv:
-#             cost = float(cost)
-#             if "/m" in item:
-#                 name = re.findall(r'\((.*?)\)', item)[0].lower()
-#                 rate_card[name] = Edge(name=name, cost_per_meter=cost)
-#             else:
-#                 node_type = item.lower()
-#                 rate_card[node_type] = Node(node_type=node_type, cost=cost)
-#     return rate_card
-
-
-if __name__ == '__main__':
-    print(generate_rate_card("rate_card_a.csv"))
